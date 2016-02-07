74585ed70af7a3c91ee11372762f044d0f9c06b6
deajakovl12 <brutallion1@gmail.com>
2016-02-07 21:54:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/deean/medix/Prijava.java
@@ -0,0 +1,13 @@
+package com.example.deean.medix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Prijava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prijava);
+    }
+}
