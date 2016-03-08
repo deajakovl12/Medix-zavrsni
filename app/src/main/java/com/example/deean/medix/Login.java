729a84da7d5092f061c3562d5d5d8a9f60622798
deajakovl12 <brutallion1@gmail.com>
2016-03-08 01:01:33 +0100

    material drawer u izradi

--- app/src/main/java/com/example/deean/medix/Login.java
+++ app/src/main/java/com/example/deean/medix/Login.java
@@ -2,12 +2,22 @@ package com.example.deean.medix;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class Login extends AppCompatActivity  implements View.OnClickListener{
     Button bPrijava;
@@ -17,6 +27,7 @@ public class Login extends AppCompatActivity  implements View.OnClickListener{
     DoktorLokalno DoktorLokalno;
     PacijentLokalno PacijentLokalno;
 
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +48,43 @@ public class Login extends AppCompatActivity  implements View.OnClickListener{
         DoktorLokalno = new DoktorLokalno(this);
         PacijentLokalno = new PacijentLokalno(this);
 
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setPadding(0, 0, 0, 0);
+        toolbar.setBackgroundColor(Color.rgb(162, 32, 34));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.app_name);
+
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("Početni").withIcon(getResources().getDrawable(R.drawable.home_small_white));
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName("O Medix app").withIcon(getResources().getDrawable(R.drawable.information_white));
+        SecondaryDrawerItem item3 = new SecondaryDrawerItem().withName("Pomoć").withIcon(getResources().getDrawable(R.drawable.help_small_white));
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        item3
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        switch (position) {
+                            case 0:
+                                //startActivity(new Intent(getApplicationContext(),Login.class));
+                                //ne treba jer kad kliknem pocetni sam se makne
+                                break;
+
+                        }
+
+                        Log.e("Pozicija", String.valueOf(position));
+                        return false;
+                    }
+                })
+                .build();
     }
 
     @Override
